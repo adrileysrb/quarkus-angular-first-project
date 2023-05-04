6ccd1bf82cec41853d7bd67669143cfe05806db8
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2023-05-04 04:45:37 -0300

    started dialog for artist table

---
+++ web/angular/src/app/artists/artist-dialog/artist-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-dialog',
+  templateUrl: './artist-dialog.component.html',
+  styleUrls: ['./artist-dialog.component.css']
+})
+export class ArtistDialogComponent {
+
+}
